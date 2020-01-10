910471f11220cdec8821e8989c16ff2c5cdb8003
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2020-01-10 14:42:52 +0700

    [#4][FEAT][SUPPLIERS] add method on supplier model

--- models/supplier.go
+++ models/supplier.go
@@ -3,6 +3,7 @@ package models
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/jacky-htg/inventory/libraries/api"
 )
@@ -43,7 +44,100 @@ func (u *Supplier) getArgs() []interface{} {
 	return args
 }
 
+// List of suppliers
+func (u *Supplier) List(ctx context.Context, db *sql.DB) ([]Supplier, error) {
+	list := []Supplier{}
+
+	rows, err := db.QueryContext(ctx, qSuppliers+" WHERE companies.id=?", ctx.Value(api.Ctx("auth")).(User).Company.ID)
+	if err != nil {
+		return list, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var s Supplier
+		err = rows.Scan(s.getArgs()...)
+		if err != nil {
+			return list, err
+		}
+
+		list = append(list, s)
+	}
+
+	if err := rows.Err(); err != nil {
+		return list, err
+	}
+
+	if len(list) <= 0 {
+		return list, errors.New("Supplier not found")
+	}
+
+	return list, nil
+}
+
 // Get supplier by id
 func (u *Supplier) Get(ctx context.Context, tx *sql.Tx) error {
 	return tx.QueryRowContext(ctx, qSuppliers+" WHERE suppliers.id=? AND companies.id=?", u.ID, ctx.Value(api.Ctx("auth")).(User).Company.ID).Scan(u.getArgs()...)
+}
+
+// Create new supplier
+func (u *Supplier) Create(ctx context.Context, db *sql.DB) error {
+	const query = `
+		INSERT INTO suppliers (company_id, code, name, address, created)
+		VALUES (?, ?, ?, ?, NOW())
+	`
+	stmt, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(ctx, ctx.Value(api.Ctx("auth")).(User).Company.ID, u.Code, u.Name, u.Address)
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	u.ID = uint64(id)
+
+	return nil
+}
+
+// Update supplier
+func (u *Supplier) Update(ctx context.Context, db *sql.DB) error {
+
+	stmt, err := db.PrepareContext(ctx, `
+		UPDATE suppliers 
+		SET name = ?,
+			address = ?,
+			updated = NOW()
+		WHERE id = ? AND company_id = ?
+	`)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, u.Name, u.Address, u.ID, ctx.Value(api.Ctx("auth")).(User).Company.ID)
+	return err
+}
+
+// Delete supplier
+func (u *Supplier) Delete(ctx context.Context, db *sql.DB) error {
+	stmt, err := db.PrepareContext(ctx, `DELETE FROM suppliers WHERE id = ? AND company_id = ?`)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, u.ID, ctx.Value(api.Ctx("auth")).(User).Company.ID)
+	return err
 }
